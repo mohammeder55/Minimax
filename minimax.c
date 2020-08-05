587cca8acd49f0d8d06346171b48e27cee0dd069
Mohammed <Mohammeder55@gmail.com>
2020-08-05 14:44:00 +0300

    - Implemented copmpose status

--- minimax.c
+++ minimax.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 void read_input(char board[], char *file_name);
 int board_status(char board[]);
@@ -13,15 +12,40 @@ int main(int argc, char *argv[]) {
     char board[10];
     read_input(board, argv[1]);
 
-    board_status(board);
-
-    // printf("%s", board);
+    int status = board_status(board);
+    printf("%04i\n", status);
 
 }
 
 int compose_status(char player, int positions[])
 {
+    int player_number;
+
+    if (player == '-')
+    {
+        return -1;
+    }
+    else if (player == '|')
+    {
+        return 0;
+    }
     
+    else
+    {
+        if (player == 'X')
+        {
+            player_number = 1;
+        }
+        else if (player == 'O')
+        {
+            player_number = 2;
+        }
+
+        return 1000 * positions[0]
+            + 100 * positions[1]
+            + 10 * positions[2]
+            + player_number;
+    }
 }
 
 int board_status(char board[])
@@ -32,6 +56,8 @@ int board_status(char board[])
         if (board[i] == board[i + 1] && board[i] == board[i + 2] && board[i] != ' ')
         {
             printf("%c %i %i %i\n", board[i], i, i + 1, i + 2);
+            int positions[3] = {i, i + 1, i + 2};
+            return compose_status(board[i], positions);
         }
     }
 
@@ -41,6 +67,9 @@ int board_status(char board[])
         if (board[i] == board[i + 3] && board[i] == board[i + 6] && board[i] != ' ')
         {
             printf("%c %i %i %i\n", board[i], i, i + 3, i + 6);
+
+            int positions[3] = {i, i + 3, i + 6};
+            return compose_status(board[i], positions);
         }
     }
 
@@ -48,25 +77,33 @@ int board_status(char board[])
     if (board[0] == board[4] && board[0] == board[8] && board[4] != ' ')
     {
         printf("%c 0 4 8\n", board[4]);
+
+        int positions[3] = {0, 4, 8};
+        return compose_status(board[4], positions);
     }
     if (board[2] == board[4] && board[2] == board[6] && board[4] != ' ')
     {
         printf("%c 2 4 6\n", board[4]);
+
+        int positions[3] = {2, 4, 6};
+        return compose_status(board[4], positions);
     }
 
-    // Check for tie
+    int positions[3] = {0, 0, 0};
+
+    // Check for empty spot
     for (int i = 0; board[i] != '\0'; i++)
     {
         if (board[i] == ' ')
         {
             printf("continue\n");
-            return 0;
+
+            return compose_status('|', positions);
         }
     }
 
     printf("tie\n");
-
-    return 0;
+    return compose_status('-', positions);
 }
 
 void read_input(char board[], char *file_name)
